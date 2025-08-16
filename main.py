7d914572833ef55e636e8cc9c145fed7b1f68c6f
MrOtonashi <dany-1765@outlook.com>
2025-08-15 20:52:02 -0400

    We can get Account Data

--- main.py
+++ main.py
@@ -2,15 +2,36 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+import requests
 
 load_dotenv() 
 TOKEN = os.getenv("DISCORD_TOKEN")
+api_key = os.getenv("RIOT_API_KEY")
+
+
+if not api_key:
+    raise RuntimeError("RIOT_API_KEY not found. Put it in your .env file.")
 
 
 intents = discord.Intents.default()
 intents.message_content = True 
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+
+game_name = "MPGGLOL"
+tag_line = "KIRYU"
+region = "europe"
+
+
+# Build request
+url = f"https://{region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}"
+headers = {"X-Riot-Token": api_key}
+
+# Fetch data
+response = requests.get(url, headers=headers)
+
+
 
 @bot.event
 async def on_ready():
@@ -19,6 +40,18 @@ async def on_ready():
 @bot.command()
 async def hello(ctx):
     await ctx.send("Hello! I'm alive.")
+
+@bot.command()
+async def mpggstats(ctx):
+    await ctx.send("Fetching MPGG stats...")
+    if response.status_code == 200:
+        data = response.json()
+        print("Account data:", data)
+        await ctx.send(f"Account data: {data}")
+    else:
+        print(f"Error {response.status_code}: {response.text}")
+        await ctx.send(f"Error {response.status_code}: {response.text}")
+    
 
 if __name__ == "__main__":
     if not TOKEN:
